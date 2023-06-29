1fd376830a25e84ce7201e0bc5bc7cd7e59d2d30
leulseged tilahun <leulseged3@gmail.com>
2023-06-29 15:51:38 +0300

    🗃️ create books entity

---
+++ src/entities/book.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  discount: number;
+
+  @Column()
+  coverImage: string;
+
+  @Column()
+  price: number
+}
